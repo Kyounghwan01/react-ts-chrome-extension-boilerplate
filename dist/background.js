df41bfecc714252eac2e0585e167f1c8e7d896ac
Kyounghwan01 <noh5524@gmail.com>
2022-09-18 22:41:40 +0900

    background script setting

---
+++ dist/background.js
@@ -0,0 +1,30 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
+  !*** ./src/background/background.ts ***!
+  \**************************************/
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("크롬익스텐션 설치됨");
+});
+
+chrome.bookmarks.onCreated.addListener(() => {
+  console.log("북마크 생성시 실행됨");
+});
+
+chrome.bookmarks.onChanged.addListener(() => {
+  console.log("북마크 수정시 실행됨");
+});
+
+chrome.bookmarks.onRemoved.addListener(() => {
+  console.log("북마크 삭제시 실행됨");
+});
+
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//   console.log(msg);
+//   console.log(sender);
+//   sendResponse("Front the background Script");
+// })
+
+/******/ })()
+;
+//# sourceMappingURL=background.js.map
---
+++ src/background/background.ts
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("크롬익스텐션 설치됨");
+});
+
+chrome.bookmarks.onCreated.addListener(() => {
+  console.log("북마크 생성시 실행됨");
+});
+
+chrome.bookmarks.onChanged.addListener(() => {
+  console.log("북마크 수정시 실행됨");
+});
+
+chrome.bookmarks.onRemoved.addListener(() => {
+  console.log("북마크 삭제시 실행됨");
+});
+
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//   console.log(msg);
+//   console.log(sender);
+//   sendResponse("Front the background Script");
+// })
